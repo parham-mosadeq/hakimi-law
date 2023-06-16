cc4c8d51082b47088fc48f54ee4e9ee6ea48d5ca
parham mosadeqzadeh <pmosadeqzadeh@gmail.com>
2023-06-16 20:04:32 +0430

    add padding to hc

--- src/components/modules/home/HomeCards.js
+++ src/components/modules/home/HomeCards.js
@@ -3,7 +3,7 @@ function HomePageCards() {
   const cardTitleClasses = 'text-xl font-serif font-bold';
   const cardTxtClasses = 'pr-4 my-4';
   return (
-    <div className=' md:grid md:gap-10 md:grid-cols-2 lg:grid-cols-2 max-w-5xl mx-auto'>
+    <div className='px-4 md:grid md:gap-10 md:grid-cols-2 lg:grid-cols-2 max-w-5xl mx-auto'>
       <div className={cardClasses}>
         <h3 className={cardTitleClasses}>موسسه حقوقی حکیمی نژاد </h3>
         <p className={cardTxtClasses}>
