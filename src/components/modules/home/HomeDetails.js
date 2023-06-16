0caf6888f321521f5b0b9ba7d4a68a72ab96e213
parham mosadeqzadeh <pmosadeqzadeh@gmail.com>
2023-06-16 20:17:27 +0430

    add grid

--- src/components/modules/home/HomeDetails.js
+++ src/components/modules/home/HomeDetails.js
@@ -5,66 +5,85 @@ function HomeDetailsPage() {
   const cardTitleClasses = 'text-xl font-serif font-bold';
   const cardTxtClasses = 'pr-4 my-4 w-3/4 mx-auto';
   return (
-    <div>
+    <article>
       {/* box */}
       <div className='h-32 bg-main_logo_color md:h-24'></div>
       {/* box */}
-
-      {/* fastest */}
-      <div className={cardClasses}>
-        <div className='flex items-center justify-start'>
-          <Image
-            className='text-main_logo_color'
-            src='/icons/magnifying-glass.svg'
-            width={30}
-            height={30}
-          />
-          <h3 className={cardTitleClasses}>دقیق </h3>
+      <article className='md:grid md:grid-cols-2 max-w-5xl mx-auto'>
+        {/* fastest */}
+        <div className={cardClasses}>
+          <div className='flex items-center justify-start'>
+            <Image
+              className='text-main_logo_color'
+              src='/icons/magnifying-glass.svg'
+              width={30}
+              height={30}
+            />
+            <h3 className={cardTitleClasses}>دقیق </h3>
+          </div>
+          <p className={cardTxtClasses}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
+            exercitationem illo hic est sunt iure natus, error dolorum voluptate
+            quasi modi quidem fuga id atque culpa doloribus ea maiores cum.
+          </p>
         </div>
-        <p className={cardTxtClasses}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
-          exercitationem illo hic est sunt iure natus, error dolorum voluptate
-          quasi modi quidem fuga id atque culpa doloribus ea maiores cum.
-        </p>
-      </div>
-      {/* fastest */}
-      {/* calender */}
-      <div className={cardClasses}>
-        <div className='flex items-center justify-start'>
-          <Image
-            className='text-main_logo_color'
-            src='/icons/calender.svg'
-            width={30}
-            height={30}
-          />
-          <h3 className={cardTitleClasses}>در سریع ترین زمان </h3>
+        {/* fastest */}
+        {/* calender */}
+        <div className={cardClasses}>
+          <div className='flex items-center justify-start'>
+            <Image
+              className='text-main_logo_color'
+              src='/icons/calender.svg'
+              width={30}
+              height={30}
+            />
+            <h3 className={cardTitleClasses}>در سریع ترین زمان </h3>
+          </div>
+          <p className={cardTxtClasses}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
+            exercitationem illo hic est sunt iure natus, error dolorum voluptate
+            quasi modi quidem fuga id atque culpa doloribus ea maiores cum.
+          </p>
         </div>
-        <p className={cardTxtClasses}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
-          exercitationem illo hic est sunt iure natus, error dolorum voluptate
-          quasi modi quidem fuga id atque culpa doloribus ea maiores cum.
-        </p>
-      </div>
-      {/* calender */}
-      {/* calender */}
-      <div className={cardClasses}>
-        <div className='flex items-center justify-start'>
-          <Image
-            className='text-main_logo_color'
-            src='/icons/trust-wallet.svg'
-            width={30}
-            height={30}
-          />
-          <h3 className={cardTitleClasses}>در سریع ترین زمان </h3>
+        {/* calender */}
+        {/* calender */}
+        <div className={cardClasses}>
+          <div className='flex items-center justify-start'>
+            <Image
+              className='text-main_logo_color'
+              src='/icons/trust-wallet.svg'
+              width={30}
+              height={30}
+            />
+            <h3 className={cardTitleClasses}>در سریع ترین زمان </h3>
+          </div>
+          <p className={cardTxtClasses}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
+            exercitationem illo hic est sunt iure natus, error dolorum voluptate
+            quasi modi quidem fuga id atque culpa doloribus ea maiores cum.
+          </p>
         </div>
-        <p className={cardTxtClasses}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
-          exercitationem illo hic est sunt iure natus, error dolorum voluptate
-          quasi modi quidem fuga id atque culpa doloribus ea maiores cum.
-        </p>
-      </div>
-      {/* calender */}
-    </div>
+        {/* calender */}
+        {/* calender */}
+        <div className={cardClasses}>
+          <div className='flex items-center justify-start'>
+            <Image
+              className='text-main_logo_color'
+              src='/icons/trust-wallet.svg'
+              width={30}
+              height={30}
+            />
+            <h3 className={cardTitleClasses}>در سریع ترین زمان </h3>
+          </div>
+          <p className={cardTxtClasses}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
+            exercitationem illo hic est sunt iure natus, error dolorum voluptate
+            quasi modi quidem fuga id atque culpa doloribus ea maiores cum.
+          </p>
+        </div>
+        {/* calender */}
+      </article>
+    </article>
   );
 }
 
