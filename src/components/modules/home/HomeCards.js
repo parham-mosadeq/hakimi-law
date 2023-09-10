c00d9b1693e55a26d557845971d94f981ad53735
parham mosadeqzadeh <pmosadeqzadeh@gmail.com>
2023-09-10 23:51:09 +0430

    update homeCard data

--- src/components/modules/home/HomeCards.js
+++ src/components/modules/home/HomeCards.js
@@ -4,7 +4,7 @@ function HomePageCards() {
   const cardClasses = 'bg-main_logo_color rounded-lg px-3 py-2 my-9';
   const cardTitleClasses = 'text-xl font-serif font-bold';
   const cardTxtClasses = 'pr-4 my-4';
-  
+
   return (
     <section className='px-4 md:grid md:gap-10 md:grid-cols-2 lg:grid-cols-2 max-w-5xl mx-auto'>
       <motion.div
@@ -13,11 +13,12 @@ function HomePageCards() {
         whileInView={{ opacity: 1 }}
         className={cardClasses}
       >
-        <h3 className={cardTitleClasses}>موسسه حقوقی حکیمی نژاد </h3>
+        <h3 className={cardTitleClasses}>
+          دپارتمان تخصصی در امور دیوان عالی کشور{' '}
+        </h3>
         <p className={cardTxtClasses}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
-          exercitationem illo hic est sunt iure natus, error dolorum voluptate
-          quasi modi quidem fuga id atque culpa doloribus ea maiores cum.
+          دارای متخصصین حوضه های امور داخلی شامل دیوان عدالت اداری، دادگستری و
+          دادگاه انقلاب اسلامی
         </p>
       </motion.div>
       <motion.div
@@ -26,11 +27,12 @@ function HomePageCards() {
         whileInView={{ opacity: 1 }}
         className={cardClasses}
       >
-        <h3 className={cardTitleClasses}>موسسه حقوقی حکیمی نژاد </h3>
+        <h3 className={cardTitleClasses}>
+          دپارتمان تخصصی داوری و دعاوی بین‌المللی
+        </h3>
         <p className={cardTxtClasses}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
-          exercitationem illo hic est sunt iure natus, error dolorum voluptate
-          quasi modi quidem fuga id atque culpa doloribus ea maiores cum.
+          انجام و ارائه خدمات حقوقی و دعاوی بین‌المللی با کمک متخصصان به نام در
+          تمامی زمینه ها. در کشورهای آمریکا، کانادا ،اروپا و آمریکای جنوبی
         </p>
       </motion.div>
       <motion.div
@@ -39,11 +41,9 @@ function HomePageCards() {
         whileInView={{ opacity: 1 }}
         className={cardClasses}
       >
-        <h3 className={cardTitleClasses}>موسسه حقوقی حکیمی نژاد </h3>
+        <h3 className={cardTitleClasses}>دپارتمان تخصصی در امور خانواده</h3>
         <p className={cardTxtClasses}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
-          exercitationem illo hic est sunt iure natus, error dolorum voluptate
-          quasi modi quidem fuga id atque culpa doloribus ea maiores cum.
+          انجام تمام امور مربوط به خانوار شامل طلاق و ازدواج ، دعاوی وراثت
         </p>
       </motion.div>
       <motion.div
@@ -52,11 +52,12 @@ function HomePageCards() {
         whileInView={{ opacity: 1 }}
         className={cardClasses}
       >
-        <h3 className={cardTitleClasses}>موسسه حقوقی حکیمی نژاد </h3>
+        <h3 className={cardTitleClasses}>
+          دپارتمان تخصصی در امور گمرکی و ثبت اسناد
+        </h3>
         <p className={cardTxtClasses}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
-          exercitationem illo hic est sunt iure natus, error dolorum voluptate
-          quasi modi quidem fuga id atque culpa doloribus ea maiores cum.
+          انجام تمامی اموری گمرکی اعم از صادرات واردات ، ترخیص کالا. و انجام
+          امور ثبت شرکت و ثبت برند و علام تجاری در خارج و داخل ایران
         </p>
       </motion.div>
     </section>
