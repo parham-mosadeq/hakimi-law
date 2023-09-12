28aa3d865e4a3845b821f8a133d30d749d2e5941
parham mosadeqzadeh <pmosadeqzadeh@gmail.com>
2023-09-13 02:10:08 +0430

    add head to lawyers page

--- src/pages/lawyers/index.js
+++ src/pages/lawyers/index.js
@@ -1,7 +1,27 @@
 import LawyersPage from '@/components/templates/LawyersPage';
+import Head from 'next/head';
 export default function Lawyers() {
   return (
     <div>
+      <Head>
+        <title>وکلا | موسسه حقوقی داوری بین اللمللی حکیمی‌نژاد</title>
+        <meta charset='utf-8' />
+        <meta
+          name='google-site-verification'
+          content='+nxGUDJ4QpAZ5l9Bsjdi102tLVC21AIh5d1Nl23908vVuFHs34='
+        ></meta>
+        <meta
+          name='description'
+          content='همکاران و وکلای مشفول به کار در موسسه حقوقی حکیمی نژاد'
+        />
+        <meta property='og:title' content='اسامی وکلای موسسه حکیمی نژاد' />
+        <meta
+          property='og:description'
+          content='اسامی وکلای موسسه حکیمی نژاد'
+        />
+        <meta property='og:image' content='/icons/landing-page-logo.png' />
+        {/* <meta name="robots" content="noindex,nofollow"/> */}
+      </Head>
       <LawyersPage />
     </div>
   );
