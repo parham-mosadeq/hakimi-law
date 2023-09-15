621e099eb6b84962cf9a6fb618877df426197808
parham mosadeqzadeh <pmosadeqzadeh@gmail.com>
2023-09-15 22:40:32 +0430

    update

--- src/components/templates/BlogsPage.js
+++ src/components/templates/BlogsPage.js
@@ -2,6 +2,8 @@ import Filters from '../modules/blogs/Filters';
 import { gql, useQuery } from '@apollo/client';
 import BlogCards from '../modules/blogs/BlogCards';
 import Loader from '../modules/blogs/Loader';
+import { useState } from 'react';
+import Pagination from '../modules/blogs/Pagination';
 
 const Query = gql`
   query Blog {
@@ -25,7 +27,9 @@ const Query = gql`
 
 export default function BlogsPage() {
   const { data, loading, error } = useQuery(Query);
-
+  // const [currentPage, setCurrentPage] = useState(3);
+  // const postPerPage = 3;
+  // const nextPage = currentPage * postPerPage;
   if (loading) {
     return (
       <>
@@ -37,24 +41,42 @@ export default function BlogsPage() {
   if (error) {
     return <p>در دست تعمیر</p>;
   }
+  const start = data.blog;
 
   return (
-    <section className='grid grid-cols-1 md:grid-cols-3 gap-5 mx-auto relative min-h-screen max-w-6xl'>
-      <div className='row-span-6'>
-        <Filters />
-      </div>
-      {/* articles */}
-      {data ? (
-        data.blog.map((blog) => {
-          return (
-            <div key={blog.id}>
-              <BlogCards blog={blog} />
-            </div>
-          );
-        })
-      ) : (
-        <></>
-      )}
-    </section>
+    <>
+      <section className='grid grid-cols-1 md:grid-cols-3 gap-5 mx-auto relative min-h-screen max-w-6xl'>
+        <div className='row-span-6'>
+          <Filters />
+        </div>
+        {/* articles */}
+        {data ? (
+          start.map((blog) => {
+            return (
+              <div key={blog.id}>
+                <BlogCards blog={blog} />
+              </div>
+            );
+          })
+        ) : (
+          <></>
+        )}
+
+        {/* <Pagination blog={data.blog} /> */}
+      </section>
+      {/* <button
+        onClick={() => setCurrentPage((prev) => prev + 1)}
+        className='bg-main_txt_color px-4 py-2 m-3 rounded-md  text-main_logo_color'
+      >
+        بعدی
+      </button>
+      {currentPage}
+      <button
+        onClick={() => setCurrentPage((prev) => prev - 1)}
+        className='bg-main_txt_color px-4 py-2 m-3 rounded-md  text-main_logo_color'
+      >
+        قبلی
+      </button> */}
+    </>
   );
 }
--- src/pages/blogs/tags/[tags].js
+++ src/pages/blogs/tags/[tags].js
@@ -1,5 +1,10 @@
+import BlogTagsPage from '@/components/templates/BlogTagsPage';
 import React from 'react';
 
 export default function BlogTags() {
-  return <div>BlogTags</div>;
+  return (
+    <div>
+      <BlogTagsPage />{' '}
+    </div>
+  );
 }
