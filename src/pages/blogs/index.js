b9fa7a6dcc3a29f154ecb9917c2e1da00d920e94
parham mosadeqzadeh <pmosadeqzadeh@gmail.com>
2023-09-18 02:15:42 +0430

    add fave icon

--- src/pages/blogs/index.js
+++ src/pages/blogs/index.js
@@ -22,6 +22,24 @@ function Blogs() {
         />
         <meta property='og:image' content='/icons/landing-page-logo.png' />
         <meta name='robots' content='index,follow' />
+        <link
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='/icons/apple-touch-icon.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='/icons/favicon-32x32.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='/icons/favicon-16x16.png'
+        />
+        <link rel='manifest' href='/icons/site.webmanifest' />
       </Head>
       <div className=''>
         <BlogsPage />
--- src/pages/contact-us/index.js
+++ src/pages/contact-us/index.js
@@ -15,16 +15,31 @@ function ContactUs() {
           name='description'
           content='راه های ارتباطی با موسسه ما به شرح زیر میباشد'
         />
-        <meta
-          property='og:title'
-          content='با تماس بگیرید'
-        />
+        <meta property='og:title' content='با تماس بگیرید' />
         <meta
           property='og:description'
           content='از طریق ایمیل و شماره تماس های زیر با تماس بگیرید'
         />
         <meta property='og:image' content='/icons/landing-page-logo.png' />
-        <meta name="robots" content="noindex,nofollow"/>
+        <meta name='robots' content='index,follow' />
+        <link
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='/icons/apple-touch-icon.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='/icons/favicon-32x32.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='/icons/favicon-16x16.png'
+        />
+        <link rel='manifest' href='/icons/site.webmanifest' />
       </Head>
       <ContactUsPage />
     </main>
