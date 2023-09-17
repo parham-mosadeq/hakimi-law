5dbb12aa6909c749c39f9b7c0b503432019bea0f
parham mosadeqzadeh <pmosadeqzadeh@gmail.com>
2023-09-18 01:57:40 +0430

    add sitemap and robots

--- src/components/templates/BlogsPage.js
+++ src/components/templates/BlogsPage.js
@@ -1,35 +1,15 @@
 import Filters from '../modules/blogs/Filters';
-import { gql, useQuery } from '@apollo/client';
+import { useQuery } from '@apollo/client';
 import BlogCards from '../modules/blogs/BlogCards';
 import Loader from '../modules/blogs/Loader';
-import { useState } from 'react';
 import Pagination from '../modules/blogs/Pagination';
+import { Query } from '../../../lib/lib';
 
-const Query = gql`
-  query Blog {
-    blog {
-      author
-      createdAt
-      description {
-        markdown
-      }
-      images {
-        url
-      }
-      slug
-      title
-      shortDesc
-      id
-      tags
-    }
-  }
-`;
+export let dataSavedFromQuery = [];
 
 export default function BlogsPage() {
   const { data, loading, error } = useQuery(Query);
-  // const [currentPage, setCurrentPage] = useState(3);
-  // const postPerPage = 3;
-  // const nextPage = currentPage * postPerPage;
+
   if (loading) {
     return (
       <>
@@ -42,7 +22,6 @@ export default function BlogsPage() {
     return <p>در دست تعمیر</p>;
   }
   const start = data.blog;
-
   return (
     <>
       <section className='grid grid-cols-1 md:grid-cols-3 gap-5 mx-auto relative min-h-screen max-w-6xl'>
---
+++ src/pages/sitemap.xml.js
@@ -0,0 +1,79 @@
+const EXTERNAL_DATA_URL = 'https://hakiminejad.com/blogs';
+
+function generateSiteMap(posts) {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+     <!--We manually set the two URLs we know already-->
+     <url>
+       <loc>https://hakiminejad.com</loc>
+     </url>
+     <url>
+       <loc>https://hakiminejad.com/blogs</loc>
+     </url>
+     ${posts
+       .map(({ title, createdAt }) => {
+         return `
+       <url>
+           <loc>${`${EXTERNAL_DATA_URL}/${title}`}</loc>
+           <lastmod>${createdAt}</lastmod>
+       </url>
+     `;
+       })
+       .join('')}
+   </urlset>
+ `;
+}
+
+export async function SiteMap() {
+  // getServerSideProps will do the heavy lifting
+}
+
+export async function getServerSideProps({ res }) {
+  // We make an API call to gather the URLs for our site
+  const request = await fetch(
+    'https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/clmj6wxqg1kjz01tccrua7fso/master',
+    {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        query: `
+  query Blog {
+    blog {
+      author
+      createdAt
+      description {
+        markdown
+      }
+      images {
+        url
+      }
+      slug
+      title
+      shortDesc
+      id
+      tags
+    }
+  }
+`,
+      }),
+    }
+  );
+  const posts = await request.json();
+  const {
+    data: { blog },
+  } = await posts;
+  console.log(blog);
+
+  // We generate the XML sitemap with the posts data
+  const sitemap = generateSiteMap(blog);
+
+  res.setHeader('Content-Type', 'text/xml');
+  // we send the XML to the browser
+  res.write(sitemap);
+  res.end();
+  return {
+    props: {},
+  };
+}
+
+export default SiteMap;
