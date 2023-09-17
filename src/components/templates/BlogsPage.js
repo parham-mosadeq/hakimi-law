366bd8499386ac892ef322d4ae9af25207c65a7f
parham mosadeqzadeh <pmosadeqzadeh@gmail.com>
2023-09-18 02:07:22 +0430

    add comment

--- src/components/templates/BlogsPage.js
+++ src/components/templates/BlogsPage.js
@@ -5,8 +5,6 @@ import Loader from '../modules/blogs/Loader';
 import Pagination from '../modules/blogs/Pagination';
 import { Query } from '../../../lib/lib';
 
-export let dataSavedFromQuery = [];
-
 export default function BlogsPage() {
   const { data, loading, error } = useQuery(Query);
 
@@ -22,6 +20,8 @@ export default function BlogsPage() {
     return <p>در دست تعمیر</p>;
   }
   const start = data.blog;
+  // check for tags
+  console.log(start);
   return (
     <>
       <section className='grid grid-cols-1 md:grid-cols-3 gap-5 mx-auto relative min-h-screen max-w-6xl'>
