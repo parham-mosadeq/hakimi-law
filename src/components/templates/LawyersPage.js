4d084013567c040666f07af964bafae2a59d4428
parham mosadeqzadeh <pmosadeqzadeh@gmail.com>
2023-10-11 13:36:11 +0330

    update lawyers page

--- src/components/templates/LawyersPage.js
+++ src/components/templates/LawyersPage.js
@@ -10,21 +10,21 @@ export default function LawyersPage() {
   };
   return (
     <section className='min-h-screen max-w-4xl mx-auto px-3  md:px-1'>
-      <article className='mt-10  '>
+      <article className='mt-10  lg:grid lg:grid-cols-2 gap-3'>
         {lawyers.map((i) => {
           return (
             <div
               key={i.id}
               className={`${
                 i.id % 2 === 0
-                  ? 'bg-main_logo_color flex-row-reverse'
-                  : 'bg-main_footer_color'
+                  ? 'bg-main_logo_color flex-row-reverse  '
+                  : 'bg-main_footer_color  '
               } rounded-lg w-full flex justify-between mx-auto my-6 `}
             >
               <Image
                 className='w-auto h-60 rounded-md object-cover'
-                src={i.img}
-                width={250}
+                src={i.img ? i.img : `/lawyers/${i.sex}.webp`}
+                width={350}
                 height={250}
               />
               <h2
